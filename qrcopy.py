9a4e7c35bfa52156e96a785aff804c5fd1a1d0e6
arbanhossain <arbanhossain@gmail.com>
2020-05-03 15:26:05 +0600

    now opens the folder if filename not specified

--- qrcopy.py
+++ qrcopy.py
@@ -1,10 +1,18 @@
 import http.server
 import socketserver
+import pyqrcode
+import socket
+import sys
 
 PORT = 6969
 
 req_hand = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(('', PORT), req_hand) as httpd:
-    print("Server at localhost:" + str(PORT))
+    file = ''
+    if len(sys.argv)>1: file = sys.argv[1]
+    url = f"{socket.gethostbyname(socket.gethostname())}:{PORT}/{file}"
+    qr = pyqrcode.create(url)
+    print(qr.terminal())
+    print("Server at", url)
     httpd.serve_forever()
